ceeb042bfa375ce3a2a190bb14f519f5316f9de3
Nyoman Triandi <andraemail.com@gmail.com>
2026-05-14 15:24:44 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+
+var numberofdrum = document.querySelectorAll(".drum").length;
+
+for (i = 0; i <= numberofdrum; i++){
+document.querySelectorAll("button")[i].addEventListener("click",clicked);
+}
+
+function clicked (){
+
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+    
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var kickbass = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        
+    }
+}
+
+
+
+
+var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
