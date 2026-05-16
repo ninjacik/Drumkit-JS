3715d779d322088b71ff4f9ad860390c4d44be6d
Nyoman Triandi <andraemail.com@gmail.com>
2026-05-16 20:38:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
-
-
 var numberofdrum = document.querySelectorAll(".drum").length;
 
-for (i = 0; i <= numberofdrum; i++){
+for (i = 0; i < numberofdrum; i++){
 document.querySelectorAll("button")[i].addEventListener("click",clicked);
 }
 
@@ -14,39 +11,34 @@ function clicked (){
     switch (buttonInnerHTML) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            audio.play();
+            tom1.play();
             break;
     
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
-            audio.play();
+            tom2.play();
             break;
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3");
-            audio.play();
+            tom3.play();
             break;
         case "d":
             var tom4 = new Audio("sounds/tom-4.mp3");
-            audio.play();
+            tom4.play();
             break;
         case "j":
             var snare = new Audio("sounds/snare.mp3");
-            audio.play();
+            snare.play();
             break;
         case "k":
             var crash = new Audio("sounds/crash.mp3");
-            audio.play();
+            crash.play();
             break;
         case "l":
             var kickbass = new Audio("sounds/kick-bass.mp3");
-            audio.play();
+            kickbass.play();
             break;
-        
+
+        default: console.log("innerHTML");
     }
 }
-
-
-
-
-var audio = new Audio("sounds/kick-bass.mp3");
-    audio.play();
